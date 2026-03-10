f6469d85028dae71f958b1a9ecadcd50984c225d
amandinederocca <amandine.de-rocca@etu.minesparis.psl.eu>
2026-03-10 11:22:08 +0100

    tic tac toe amélioré

--- ttt/tic_tac_toe.js
+++ ttt/tic_tac_toe.js
@@ -1,17 +1,16 @@
 'use strict';
 
-function check_win(color) {
-	/* durty check with pattern */
+function gagné(color) {
 	let grid_data = "";
 	for (let i = 0; i < 3; i++) {
 		for (let j = 0; j < 3; j++) {
-			let c = document.getElementById(`cell-${i}-${j}`);
+			let c = document.getElementById(`case-${i}-${j}`);
 			grid_data = grid_data+ (c.classList.contains(color)?"1":"0");
 		}
 	}
 
 	if (
-		grid_data.match(/111....../) ||
+		grid_data.match(/111....../) ||//on regarde si une des combinaisons qui permet de gagner est réalisé
 		grid_data.match(/...111.../) ||
 		grid_data.match(/......111/) ||
 		grid_data.match(/1..1..1../) ||
@@ -26,49 +25,49 @@ function check_win(color) {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-	let current_color = 'red';
+	let current_color = 'rouge';
 	const windlg = document.getElementById("windlg");
 	const restart_btn = document.getElementById("restart");
 	const reset_btn = document.getElementById("reset");
-	const board = document.getElementById("board");
+	const board = document.getElementById("plateau");
 
 	function reset() {
 		current_color = 'red';
 		for (let i = 0; i < 3; i++) {
 			for (let j = 0; j < 3; j++) {
-				let c = document.getElementById(`cell-${i}-${j}`);
-				c.classList.remove("red");
-				c.classList.remove("green");
+				let c = document.getElementById(`case-${i}-${j}`);
+				c.classList.remove("rouge");
+				c.classList.remove("vert");
 			}
 		}
 
-		// Hide win dialog box
+		// on cache le bouton winner
 		windlg.style.display = 'none';
 		
-		// Display board and reset button
+		// on affiche le boutton et le plateau
 		reset_btn.style.display = 'block';
-		board.style.display = 'grid';
+		plateau.style.display = 'grid';
 	}
 
 	for (let i = 0; i < 3; i++) {
 		for (let j = 0; j < 3; j++) {
-			let c = document.getElementById(`cell-${i}-${j}`);
+			let c = document.getElementById(`case-${i}-${j}`);
 			c.addEventListener('click', function() {
-				if (c.classList.contains("red") || c.classList.contains("green"))
+				if (c.classList.contains("rouge") || c.classList.contains("vert"))
 					return;
 				c.classList.add(current_color);
-				if (check_win(current_color)) {
+				if (gagné(current_color)) {
 					windlg.firstElementChild.innerHTML = `<span class="${current_color}">${current_color}</span> win!!!`;
 
-					// Show dialog box
+					// on fait apparaitre le bouton de victoire sous forme d'un bloc
 					windlg.style.display = "block";
 
-					// Hide board game
-					board.style.display = "none";
+					// et on fait disparaitre le reste
+					plateau.style.display = "none";
 					reset_btn.style.display = "none";
 					
 				}
-				current_color = current_color == 'red' ? 'green' : 'red';
+				current_color = current_color == 'rouge' ? 'vert' : 'rouge';
 			});
 		}
 	}
@@ -78,73 +77,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-
-const svgNS = "http://www.w3.org/2000/svg"
-
-
-/* generates random circles in specified area */
-class Board {
-
-  constructor(width, height, radius) {
-    this.w = width
-    this.h = height
-    this.r = radius
-    this.active = false
-  }
-
-  toggle() {
-    this.active = !this.active
-  }
-
-  add_random_circle() {
-    // create a circle
-    let c = document.createElementNS(svgNS, 'circle')
-    // change its attributes
-    let [rx, ry] = [Math.random(), Math.random()]
-    let [x, y] = [rx * this.w, ry * this.h]
-    c.setAttribute('cx', x) // center
-    c.setAttribute('cy', y)
-    c.setAttribute('r', this.r)  // radius
-    // locate the <svg> element
-    let svg = document.querySelector("svg")
-    // insert circle in <svg> element
-    svg.append(c)
-  }
-
-  // heartbeat
-  heart_beat() {
-    console.log(`in RUN, active=${this.active}`)
-    if (this.active) {
-      this.add_random_circle()
-    }
-  }
-
-  // do {this.heart_beat()} every 500 ms
-  start() {
-    // first parameter here is a function
-    // that we want to call every 500 ms
-    setInterval(() => this.heart_beat(), 500)
-  }
-}
-
-// initialize, but only once the page is loaded
-document.addEventListener('DOMContentLoaded',
-  function () {
-    // create instance
-    let the_board = new Board(200, 200, 4)
-    the_board.start()
-
-    // set svg size
-    let svg = document.querySelector("svg")
-    svg.setAttribute('width', the_board.w)
-    svg.setAttribute('height', the_board.h)
-
-    // arm callback
-    document.getElementById("button")
-      .addEventListener('click', () => the_board.toggle())
-      // note that this form would not work
-      // .addEventListener( 'click', the_board.toggle )
-      // because the 'this' variable in toggle()
-      // would reference the button element
-  })
